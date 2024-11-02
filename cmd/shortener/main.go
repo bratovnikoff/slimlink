e10974fa94ed3306aad13e2cf6d76bc3f6ca27e1
Iaroslav Bratovnikov <bratovnikoff@gmail.com>
2024-11-02 04:05:12 +0300

    Add shorten URL and resolve hashed URL

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,79 @@
 package main
 
-func main() {}
+import (
+	"io"
+	"net/http"
+
+	"github.com/sqids/sqids-go"
+)
+
+var (
+	urls map[string]string
+	s    *sqids.Sqids
+)
+
+func main() {
+	if err := run(); err != nil {
+		panic(err)
+	}
+}
+
+func run() error {
+	urls = make(map[string]string)
+	s, _ = sqids.New(sqids.Options{
+		Alphabet:  "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.+",
+		MinLength: 8,
+	})
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", checkMethod)
+
+	return http.ListenAndServe(`:8080`, mux)
+}
+
+func checkMethod(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		shortenUrl(w, r)
+	} else if r.Method == http.MethodGet {
+		getUrl(w, r)
+	}
+}
+
+func shortenUrl(w http.ResponseWriter, req *http.Request) {
+	contentType := req.Header.Get("Content-Type")
+	body, err := io.ReadAll(req.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	if req.URL.Path != "/" || contentType != "text/plain" || len(body) == 0 {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	id, err := s.Encode([]uint64{uint64(len(urls))})
+	if err != nil {
+		panic(err)
+	}
+
+	urls[id] = string(body)
+
+	w.Header().Set("Content-Type", "text/plain")
+	w.WriteHeader(http.StatusCreated)
+
+	_, _ = w.Write([]byte("http://localhost:8080/" + id + "\r\n"))
+}
+
+func getUrl(w http.ResponseWriter, req *http.Request) {
+	id := req.URL.Path[len("/"):]
+	_, ok := urls[id]
+	if id == "" || !ok {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	addr := urls[id]
+
+	w.Header().Set("Location", addr)
+	w.WriteHeader(http.StatusTemporaryRedirect)
+}
